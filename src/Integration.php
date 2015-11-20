d46e8976bbefbe2b6d849673aff89394932c97b4
Remco Tolsma <info@remcotolsma.nl>
2015-11-20 11:51:43 +0100

    Added an custom config factory.

--- src/Integration.php
+++ src/Integration.php
@@ -1,10 +1,22 @@
 <?php
 
+/**
+ * Title: Friesland Bank - iDEAL Zakelijk Plus integration
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2015
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
 class Pronamic_WP_Pay_Gateways_FrieslandBank_IDealZakelijkPlus_Integration extends Pronamic_WP_Pay_Gateways_IDealAdvanced_AbstractIntegration {
 	public function __construct() {
 		$this->id         = 'frieslandbank-ideal-zakelijk-plus';
 		$this->name       = 'Friesland Bank - iDEAL Zakelijk Plus';
 		$this->provider   = 'frieslandbank';
 		$this->deprecated = true;
+	}
+
+	public function get_config_factory_class() {
+		return 'Pronamic_WP_Pay_Gateways_FrieslandBank_IDealZakelijk_ConfigFactory';
 	}
 }
